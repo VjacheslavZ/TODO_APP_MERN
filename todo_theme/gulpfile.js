9c660f5d2d26652f0d6b664c1e7ee6895ccdea5d
VjacheslavZ <slavikov.net@mail.ru>
2018-08-19 14:40:14 +0300

    gulp remove unused tasks

--- todo_theme/gulpfile.js
+++ todo_theme/gulpfile.js
@@ -1,22 +1,16 @@
 var gulp         = require("gulp"),
     sass         = require('gulp-sass'),
     browserSync  = require('browser-sync'),
-    concat       = require("gulp-concat"),
-    uglyfi       = require("gulp-uglifyjs"),
     cssnano      = require("gulp-cssnano"),
     rename       = require("gulp-rename"),
-    delfile      = require("del"),
     imagemin     = require("gulp-imagemin"),
     pngquant     = require("imagemin-pngquant"),
     cache        = require("gulp-cache"),
     autoPrefixer = require("gulp-autoprefixer"),
     pug          = require('gulp-pug'),
     gutil        = require("gulp-util"),
-    gulpCopy    = require('gulp-copy'),
-    spritesmith = require('gulp.spritesmith');
     notify = require("gulp-notify");
     sourceMaps = require('gulp-sourcemaps');
-    gulpif = require('gulp-if');
 
 gulp.task("sass", function () {
     return gulp.src([
@@ -49,34 +43,6 @@ gulp.task("css-libs", ['sass'], function () {
         .pipe(gulp.dest("app/css"));
 });
 
-gulp.task("scripts-libs", function () {
-   return gulp.src([
-      // "app/libs/jquery/dist/jquery.min.js",
-      //  "app/libs/bootstrap/dist/js/bootstrap.js",
-      // "app/libs/owl.carousel/dist/owl.carousel.min.js",
-      // "app/libs/magnific-popup/dist/jquery.magnific-popup.min.js",
-      // "app/libs/jquery-validation/dist/jquery.validate.js",
-      //  "app/libs/HighlyCustomizable/dist/bootstrap-slider.min.js",
-      //  "app/libs/slick/slick/slick.js",
-      //  "app/libs/jQuery.equalHeights/jquery.equalheights.js",
-
-   ])
-       .pipe(concat("libs.min.js"))
-       .pipe(uglyfi())
-       .pipe(gulp.dest("app/js"));
-});
-
-gulp.task("script", function () {
-   return gulp.src([
-       "app/pages/base.js",
-       "app/pages/index/index.js",
-       "app/pages/news/news.js",
-    ])
-       .pipe(concat("common.js"))
-       .pipe(gulp.dest("app/js"))
-       .pipe(browserSync.stream())
-});
-
 gulp.task("browser-sync", function () {
     browserSync({
         server:{
@@ -87,19 +53,6 @@ gulp.task("browser-sync", function () {
     });
 });
 
-gulp.task("clean", function () {
-   return delfile.sync("dist");
-});
-
-gulp.task("clear", function () {
-    return cache.clearAll();
-});
-
-// gulp.task("img", function () {
-//     return gulp.src("app/img/**/*")
-//         .pipe(gulp.dest("public/index/img"));
-// });
-
 gulp.task("img-min", function () {
    return gulp.src("app/img/**/*.jpg")
        .pipe(cache(imagemin({
@@ -109,25 +62,6 @@ gulp.task("img-min", function () {
            une: [pngquant()]
        })))
        .pipe(gulp.dest("dist/img"));
-});
-
-gulp.task('sprite', function () {
-    var spriteData = gulp.src('app/img/sprite/*.png')
-        .pipe(spritesmith({
-            padding: 10,
-            imgName: 'sprite.png',
-            cssName: 'sprite.sass',
-            imgPath: '../img/sprite.png',
-    }));
-
-
-    var imgStream = spriteData.img
-        .pipe(gulp.dest('app/img'));
-
-    var cssStream = spriteData.css
-        .pipe(gulp.dest('app/sass'));
-
-    return merge(imgStream, cssStream)
 });
 
 gulp.task("pages", function() {
@@ -149,30 +83,9 @@ gulp.task("pages", function() {
         .pipe(browserSync.stream())
 });
 
-gulp.task("watch",[ "browser-sync", "css-libs", "script", "scripts-libs", "pages", /*,"img"*/], function () {
-
+gulp.task("watch",[ "browser-sync", "css-libs", "pages"], function () {
     gulp.watch("app/pages/**/*.pug", ['pages']);
     gulp.watch('app/sass/*.sass', ["sass"]);
     gulp.watch('app/sass/libs.sass', ["css-libs"]);//ели подключен новый плагин
-    // gulp.watch('app/img/**/*', ["img"]);
     gulp.watch("app/pages/**/*.js", ["script"]);
-
-});
-
-gulp.task("build", ["clean", "img-min", "sass", "scripts-libs", 'sprite'], function () {
-
-    var buildCss = gulp.src([
-        "app/css/main.css",
-        "app/css/libs.min.css"
-    ])
-        .pipe(gulp.dest("dist/css"));
-
-    var buildFonts = gulp.src("app/fonts/**/*")
-        .pipe(gulp.dest("dist/fonts"));
-
-    var buildJs = gulp.src("app/js/**/*")
-        .pipe(gulp.dest("dist/js"));
-
-    var buildHtml = gulp.src("app/*.html")
-        .pipe(gulp.dest("dist"));
 });
