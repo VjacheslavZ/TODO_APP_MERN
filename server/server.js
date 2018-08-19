43c0ac4b518de7f94080615e2938d0ee6321195f
VjacheslavZ <slavikov.net@mail.ru>
2018-08-19 16:52:01 +0300

    setUp keys for mongoDB, base setUp server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const users = require('./routes/api/users');
+const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use((bodyParser.json()));
+
+//app.get('/', (req, res) => res.send('Hello Dev !!'));
+
+app.use('/api/users', users);
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+	.connect(db,{ useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
